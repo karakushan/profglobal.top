2f26262706a773faf0c75ae1495682fb7026a66f
Vitaliy <karakushan@gmail.com>
2024-10-25 19:03:16 +0300

    додав блоки адсенс в середину постів

--- wp-content/themes/allium-child/archive.php
+++ wp-content/themes/allium-child/archive.php
@@ -9,63 +9,75 @@
 
 get_header(); ?>
 
-	<div class="page-header-wrapper page-header-wrapper-archive">
-		<div class="container">
+<div class="page-header-wrapper page-header-wrapper-archive">
+    <div class="container">
 
-			<div class="row">
-				<div class="col">
+        <div class="row">
+            <div class="col">
 
-					<header class="page-header">
-                        <h1 class="page-title"><?php single_cat_title(  ) ?></h1>
-					</header><!-- .page-header -->
+                <header class="page-header">
+                    <h1 class="page-title"><?php single_cat_title() ?></h1>
+                </header><!-- .page-header -->
 
-				</div><!-- .col -->
-			</div><!-- .row -->
+            </div><!-- .col -->
+        </div><!-- .row -->
 
-		</div><!-- .container -->
-	</div><!-- .page-header-wrapper -->
+    </div><!-- .container -->
+</div><!-- .page-header-wrapper -->
 
-	<div class="site-content-inside">
-		<div class="container">
-			<div class="row">
+<div class="site-content-inside">
+    <div class="container">
+        <div class="row">
 
-				<div id="primary" class="content-area <?php allium_layout_class( 'content' ); ?>">
-					<main id="main" class="site-main" role="main">
+            <div id="primary" class="content-area <?php allium_layout_class('content'); ?>">
+                <main id="main" class="site-main" role="main">
+                    <?php if (have_posts()) : ?>
 
-					<?php if ( have_posts() ) : ?>
+                        <div id="post-wrapper" class="post-wrapper post-wrapper-archive">
+                            <?php $count = 1; /* Start the Loop */ ?>
+                            <?php while (have_posts()) : the_post(); ?>
 
-						<div id="post-wrapper" class="post-wrapper post-wrapper-archive">
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
+                                <?php
+                                /* Include the Post-Format-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/content/content', get_post_format());
+                                ?>
 
-							<?php
-								/* Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'template-parts/content/content', get_post_format() );
-							?>
+                                <?php if ($count % 2 == 0 && $count > 1): ?>
+                                    <ins class="adsbygoogle"
+                                         style="display:block"
+                                         data-ad-format="fluid"
+                                         data-ad-layout-key="-di-8e+ed+it-12t"
+                                         data-ad-client="ca-pub-2704679583568814"
+                                         data-ad-slot="6212621992"></ins>
+                                    <script>
+                                        (adsbygoogle = window.adsbygoogle || []).push({});
+                                    </script>
 
-						<?php endwhile; ?>
-						</div><!-- .post-wrapper -->
+                                <?php endif ?>
+                                <?php $count++; ?>
+                            <?php endwhile; ?>
+                        </div><!-- .post-wrapper -->
 
-						<?php allium_the_posts_pagination(); ?>
+                        <?php allium_the_posts_pagination(); ?>
 
-					<?php else : ?>
+                    <?php else : ?>
 
-						<div class="post-wrapper post-wrapper-single post-wrapper-single-notfound">
-							<?php get_template_part( 'template-parts/content/content', 'none' ); ?>
-						</div><!-- .post-wrapper -->
+                        <div class="post-wrapper post-wrapper-single post-wrapper-single-notfound">
+                            <?php get_template_part('template-parts/content/content', 'none'); ?>
+                        </div><!-- .post-wrapper -->
 
-					<?php endif; ?>
+                    <?php endif; ?>
 
-					</main><!-- #main -->
-				</div><!-- #primary -->
+                </main><!-- #main -->
+            </div><!-- #primary -->
 
-				<?php get_sidebar(); ?>
+            <?php get_sidebar(); ?>
 
-			</div><!-- .row -->
-		</div><!-- .container -->
-	</div><!-- .site-content-inside -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+</div><!-- .site-content-inside -->
 
 <?php get_footer(); ?>
--- wp-content/themes/allium-child/index.php
+++ wp-content/themes/allium-child/index.php
@@ -13,65 +13,81 @@
 
 get_header(); ?>
 
-	<?php if ( ! have_posts() ) : ?>
-	<div class="page-header-wrapper page-header-wrapper-home">
-		<div class="container">
+<?php if (!have_posts()) : ?>
+    <div class="page-header-wrapper page-header-wrapper-home">
+        <div class="container">
 
-			<div class="row">
-				<div class="col">
+            <div class="row">
+                <div class="col">
 
-					<header class="page-header">
-						<?php printf( '<h1 class="page-title"><span class="page-title-label">%1$s</span></h1>', esc_html__( 'Nothing Found', 'allium' ) ); ?>
-					</header><!-- .page-header -->
+                    <header class="page-header">
+                        <?php printf('<h1 class="page-title"><span class="page-title-label">%1$s</span></h1>', esc_html__('Nothing Found', 'allium')); ?>
+                    </header><!-- .page-header -->
 
-				</div><!-- .col -->
-			</div><!-- .row -->
+                </div><!-- .col -->
+            </div><!-- .row -->
 
-		</div><!-- .container -->
-	</div><!-- .page-header-wrapper -->
-	<?php endif; ?>
+        </div><!-- .container -->
+    </div><!-- .page-header-wrapper -->
+<?php endif; ?>
 
-	<div class="site-content-inside">
-		<div class="container">
-			<div class="row">
+<div class="site-content-inside">
+    <div class="container">
+        <div class="row">
 
-				<div id="primary" class="content-area <?php allium_layout_class( 'content' ); ?>">
-					<main id="main" class="site-main" role="main">
+            <div id="primary" class="content-area <?php allium_layout_class('content'); ?>">
+                <main id="main" class="site-main" role="main">
 
-					<?php if ( have_posts() ) : ?>
+                    <?php
+                    $count = 1;
+                    if (have_posts()) : ?>
 
-						<div id="post-wrapper" class="post-wrapper post-wrapper-archive">
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
+                        <div id="post-wrapper" class="post-wrapper post-wrapper-archive">
+                            <?php /* Start the Loop */ ?>
+                            <?php while (have_posts()) : the_post(); ?>
 
-							<?php
-								/* Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'template-parts/content/content', get_post_format() );
-							?>
+                                <?php
+                                /* Include the Post-Format-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/content/content', get_post_format());
+                                ?>
 
-						<?php endwhile; ?>
-						</div><!-- .post-wrapper -->
+                                <?php if ($count % 2 == 0 && $count > 1): ?>
+                                    <ins class="adsbygoogle"
+                                         style="display:block"
+                                         data-ad-format="fluid"
+                                         data-ad-layout-key="-di-8e+ed+it-12t"
+                                         data-ad-client="ca-pub-2704679583568814"
+                                         data-ad-slot="6212621992"></ins>
+                                    <script>
+                                        (adsbygoogle = window.adsbygoogle || []).push({});
+                                    </script>
 
-						<?php allium_the_posts_pagination(); ?>
+                                <?php endif ?>
+                                <?php $count++; ?>
 
-					<?php else : ?>
+                            <?php endwhile; ?>
+                        </div><!-- .post-wrapper -->
 
-						<div class="post-wrapper post-wrapper-single post-wrapper-single-notfound">
-							<?php get_template_part( 'template-parts/content/content', 'none' ); ?>
-						</div><!-- .post-wrapper -->
+                        <?php allium_the_posts_pagination(); ?>
 
-					<?php endif; ?>
+                    <?php else : ?>
 
-					</main><!-- #main -->
-				</div><!-- #primary -->
+                        <div class="post-wrapper post-wrapper-single post-wrapper-single-notfound">
+                            <?php get_template_part('template-parts/content/content', 'none'); ?>
+                        </div><!-- .post-wrapper -->
 
-				<?php get_sidebar(); ?>
+                    <?php endif; ?>
 
-			</div><!-- .row -->
-		</div><!-- .container -->
-	</div><!-- .site-content-inside -->
+                </main><!-- #main -->
+            </div><!-- #primary -->
+
+            <?php get_sidebar(); ?>
+
+        </div><!-- .row -->
+    </div><!-- .container -->
+</div><!-- .site-content-inside -->
 
 <?php get_footer(); ?>
